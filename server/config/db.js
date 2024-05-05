3c6878dba80ec6acaf2624fa7faca07702e34709
Danny Palmeira <danny.palmeira@gmail.com>
2024-05-05 19:29:26 +0200

    Changes to be committed:

--- server/config/db.js
+++ server/config/db.js
@@ -4,10 +4,7 @@ const connectDB = async()=> {
     mongoose.set('strictQuery', false);
     const conn = await mongoose.connect(process.env.MONGODB_URI);
     console.log(`Database Connected: ${conn.connection.host}`);
-<<<<<<< HEAD
     const connection=mongoose.connection;
-=======
->>>>>>> 91f3ddd5ae7d750f4fb0f33f07a56fd7b622ff59
   } catch (error) {
     console.log(error);
   }
